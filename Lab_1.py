1e858187ce265bea6ff99ec725f46611b2d1d86a
Romo4kaaa <113985111+Romo4kaaa@users.noreply.github.com>
2024-11-06 09:31:32 +0200

    Final lab1

--- Lab_1.py
+++ Lab_1.py
@@ -3,27 +3,26 @@ from IPython.display import display
 import random
 import numpy as np
 import matplotlib.pyplot as plt
-
+#Етап 1
 N = 30
 
-# Генеруємо першу випадкову послідовність з чисел в діапазоні від 0 до 99
+# Генеруєм випадкову послідовність 0-99
 x_prob = [random.randint(0, 99) for _ in range(N)]
-y_prob = [random.choice([0, 99]) for _ in range(N)]
 
-print("Перша випадкова послідовність:", x_prob)
-print("Друга випадкова послідовність:", y_prob)
+y_prob = [random.choice([0,99]) for _ in range(N)]
+
+print("Випадкова послідовність 1:", x_prob)
+print("Випадкова послідовність 2:", y_prob)
 
 # ---------------------------
 
-def generateSequence(N:int, a0:float, d:float) -> list[float]:
+def Generacia_poslidovnosti(N:int, a0:float, d:float) -> list[float]:
     return [a0 + d * i for i in range(N)]
 
-Xdetup = generateSequence(N, np.random.randint(-N, N), np.random.randint(1, N))
-Ydetup = generateSequence(N, np.random.randint(-N, N), np.random.randint(1, N))
-
-Xdetdown = generateSequence(N, np.random.randint(-N, N), np.random.randint(-N, 0))
-Ydetdown = generateSequence(N, np.random.randint(-N, N), np.random.randint(-N, 0))
-
+Xdetup = Generacia_poslidovnosti(N, np.random.randint(-N, N), np.random.randint(1, N))
+Ydetup = Generacia_poslidovnosti(N, np.random.randint(-N, N), np.random.randint(1, N))
+Xdetdown = Generacia_poslidovnosti(N, np.random.randint(-N, N), np.random.randint(-N, 0))
+Ydetdown = Generacia_poslidovnosti(N, np.random.randint(-N, N), np.random.randint(-N, 0))
 print("Xdetup = ", Xdetup)
 print("Ydetup = ", Ydetup)
 print("Xdetdown = ", Xdetdown)
@@ -33,15 +32,13 @@ print("Ydetdown = ", Ydetdown)
 
 x_stoch1 = np.add(x_prob, Xdetup)
 x_stoch2 = np.add(x_prob, Xdetdown)
-
-# Зміна: Робимо віднімання для y_stoch2 для від’ємної кореляції
 y_stoch1 = np.add(y_prob, Ydetup)
 y_stoch2 = np.subtract(y_prob, Ydetdown)
 
 print("x_stoch1 = ", x_stoch1.tolist())
-print("x_stoch2 (від'ємна кореляція) = ", x_stoch2.tolist())
+print("x_stoch2 = ", x_stoch2.tolist())
 print("y_stoch1 = ", y_stoch1.tolist())
-print("y_stoch2 (від'ємна кореляція) = ", y_stoch2.tolist())
+print("y_stoch2 = ", y_stoch2.tolist())
 
 # Побудова графіків для x_prob та y_prob
 plt.plot(x_prob, y_prob, 'go')  # зелені точки для x_prob та y_prob
@@ -89,10 +86,6 @@ def calc(X: list[float], Y: list[float]):
 # Обчислення кореляції для всіх пар
 result = [
     calc(x_prob, y_prob),
-    calc(x_prob, x_stoch1),
-    calc(x_prob, x_stoch2),
-    calc(y_prob, y_stoch1),
-    calc(y_prob, y_stoch2),
     calc(x_stoch1, y_stoch1),
     calc(x_stoch2, y_stoch2)
 ]
