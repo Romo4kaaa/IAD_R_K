6a2ed37f3e1ded0bf030de623700d81c373e9fc6
Romo4kaaa <113985111+Romo4kaaa@users.noreply.github.com>
2024-11-27 16:06:31 +0200

    labs

--- Lab_4.py
+++ Lab_4.py
@@ -1,50 +1,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
-# Задаємо кількість точок у кожній множині
 num_points = np.random.randint(5, 11)
 
-# Задаємо математичні середні для кожної множини так, щоб утворити трикутник
 mean1 = np.array([0, 0])
 mean2 = np.array([2, 0])
 mean3 = np.array([1, 2])
 
-# Задаємо дисперсію (можна вибрати довільне значення)
 variance = 1.5
 
-# Генеруємо випадкові точки для кожної множини
 set1 = np.random.normal(mean1, variance, size=(num_points, 2))
 set2 = np.random.normal(mean2, variance, size=(num_points, 2))
 set3 = np.random.normal(mean3, variance, size=(num_points, 2))
 
-# Відображення точок на графіку
 plt.scatter(set1[:, 0], set1[:, 1], label='Set 1')
 plt.scatter(set2[:, 0], set2[:, 1], label='Set 2')
 plt.scatter(set3[:, 0], set3[:, 1], label='Set 3')
 
-# Встановлюємо відстані між математичними середніми
 plt.plot([mean1[0], mean2[0]], [mean1[1], mean2[1]], color='black')
 plt.plot([mean2[0], mean3[0]], [mean2[1], mean3[1]], color='black')
 plt.plot([mean3[0], mean1[0]], [mean3[1], mean1[1]], color='black')
 
-# Виводимо графік
 plt.title('Random Points with Triangular Centers')
 plt.legend()
 plt.show()
 
-# Початкові центри кластерів
+# ------------------------
+
 initial_centers = np.vstack([mean1, mean2, mean3])
 
-# Відображення точок та початкових центрів на графіку
 plt.scatter(set1[:, 0], set1[:, 1], label='Set 1')
 plt.scatter(set2[:, 0], set2[:, 1], label='Set 2')
 plt.scatter(set3[:, 0], set3[:, 1], label='Set 3')
 plt.scatter(initial_centers[:, 0], initial_centers[:, 1], c='red', marker='X', label='Initial Centers')
 
-# Виводимо графік
 plt.title('Random Points with Triangular Centers and Initial Cluster Centers')
 plt.legend()
 plt.show()
+
+#----------------------------
 
 def k_means_clustering(data, k, initial_centers, max_iterations=100):
     if max_iterations <= 0:
@@ -78,31 +73,27 @@ def k_means_clustering(data, k, initial_centers, max_iterations=100):
         centers = new_centers
 
     return labels, centers, step + 1
-
-# Об'єднуємо всі точки в один масив
+# Обєднуємо точки
 all_points = np.vstack([set1, set2, set3])
 
-# Визначаємо кількість кластерів та початкові центри
 k_clusters = 3
 initial_centers = np.vstack([mean1, mean2, mean3])
 
-# Застосовуємо k-means
 cluster_labels, final_centers, num_steps = k_means_clustering(all_points, k_clusters, initial_centers)
 
-# Відображення результатів кластеризації
 for i in range(k_clusters):
     cluster_points = all_points[cluster_labels == i]
     plt.scatter(cluster_points[:, 0], cluster_points[:, 1], label=f'Cluster {i + 1}')
 
-# Відображення центрів кластерів
 plt.scatter(final_centers[:, 0], final_centers[:, 1], c='red', marker='X', label='Final Centers')
 
 plt.title('k-means Clustering Results')
 plt.legend()
 plt.show()
+print(f"Алгоритм завершився на {num_steps} кроках.")
 
+#----------------------------------------------------
 
-# Генеруємо початкові центри кластерів
 initial_centers_away = np.array([mean1 + 0.75 * variance * np.random.randn(2),
                             mean2 + 0.75 * variance * np.random.randn(2),
                             mean3 + 0.75 * variance * np.random.randn(2)])
@@ -110,7 +101,6 @@ initial_centers_away = np.array([mean1 + 0.75 * variance * np.random.randn(2),
 results_away = k_means_clustering(all_points, k_clusters, initial_centers_away)
 cluster_labels_away, final_centers_away, num_steps_away = results_away
 
-# Відображення результатів кластеризації
 for i in range(k_clusters):
     cluster_points = all_points[cluster_labels_away == i]
     plt.scatter(cluster_points[:, 0], cluster_points[:, 1], label=f'Cluster {i + 1}')
@@ -118,9 +108,9 @@ plt.scatter(final_centers_away[:, 0], final_centers_away[:, 1], c='red', marker=
 plt.title('k-means Clustering Results (Initial Centers Away)')
 plt.legend()
 
-# Виводимо кількість кроків
 print(f"Алгоритм завершився на {num_steps_away} кроках.")
 
+#-----------------------------------------------------------
 
 initial_centers_towards = np.array([mean1 - 0.75 * variance * np.random.randn(2),
                             mean2 - 0.75 * variance * np.random.randn(2),
@@ -129,7 +119,6 @@ initial_centers_towards = np.array([mean1 - 0.75 * variance * np.random.randn(2)
 results_towards = k_means_clustering(all_points, k_clusters, initial_centers_towards)
 cluster_labels_towards, final_centers_towards, num_steps_towards = results_towards
 
-# Відображення результатів кластеризації
 for i in range(k_clusters):
     cluster_points = all_points[cluster_labels_towards == i]
     plt.scatter(cluster_points[:, 0], cluster_points[:, 1], label=f'Cluster {i + 1}')
@@ -139,11 +128,10 @@ plt.legend()
 
 plt.show()
 
-# Виводимо кількість кроків
 print(f"Алгоритм завершився на {num_steps} кроках.")
 
+#------------------------------------------------------------
 
-import pandas as pd
 data_comparison = {
     'Випадок': ['З трикутного розподілу','Віддалено', 'Наближено'],
     'Кількість кроків': [num_steps, num_steps_away, num_steps_towards]
@@ -152,6 +140,4 @@ data_comparison = {
 df_comparison = pd.DataFrame(data_comparison)
 df_comparison.set_index('Випадок', inplace=True)
 
-# Виведемо таблицю порівняння
 df_comparison
-
